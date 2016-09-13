a49e6928796f92df4b0954752b17571afe5b69f7
dnikolovv <dnikolovv@hotmail.com>
2016-09-13 11:52:31 +0300

    Moved views to angular folder

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ db.on('open', function (err) {
 });
 
 app.get('/partials/:partialArea/:partialName', function (request, response) {
-    response.render('partials/' + request.params.partialArea + '/' + request.params.partialName);
+    response.render('../../public/app/' + request.params.partialArea + '/' + request.params.partialName);
 });
 
 app.get('*', function (req, res) {
